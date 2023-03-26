01a1ddfce1f66f88ae9269572aa2eda17851bf1c
Hava Gundogan <havakabasgundogan@gmail.com>
2023-03-26 21:43:11 +0300

    Created Signup

--- app/src/main/java/com/gundogan/taskmanager/SignUp.java
+++ app/src/main/java/com/gundogan/taskmanager/SignUp.java
@@ -1,32 +1,71 @@
 package com.gundogan.taskmanager;
 
+import static android.content.ContentValues.TAG;
+
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+
 
 public class SignUp extends AppCompatActivity {
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up_layout);
 
-        EditText birthDateEditText = findViewById(R.id.birthDateEditText);
-        birthDateEditText.setOnClickListener(new View.OnClickListener() {
+        EditText dobEditText = findViewById(R.id.birthDateEditText);
+        dobEditText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 pickBirthDate();
             }
+
         });
+
+
+        Button signupButton = findViewById(R.id.buttonSignup);
+        signupButton.setOnClickListener(v -> {
+            EditText nameEditText = findViewById(R.id.textNameSignup);
+            EditText emailEditText = findViewById(R.id.textEmailSignup);
+            EditText passwordEditText = findViewById(R.id.textPasswordSignup);
+            EditText EditText = findViewById(R.id.birthDateEditText);
+
+            String name = nameEditText.getText().toString();
+            String email = emailEditText.getText().toString();
+            String password = passwordEditText.getText().toString();
+            String dob = EditText.getText().toString();
+
+            signup(name, email, password, dob);
+        });
+
+
 
     }
    private void pickBirthDate() {
@@ -62,5 +101,53 @@ public class SignUp extends AppCompatActivity {
        datePicker.show();
    }
 
+    private void signup(String name, String email, String password, String dob) {
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+
+                        Map<String, Object> userMap = new HashMap<>();
+                        userMap.put("name", name);
+                        userMap.put("email", email);
+                        userMap.put("dob", dob);
+
+
+
+// Add a new document with a generated ID
+                        db.collection("users")
+                                .add(userMap)
+                                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                    @Override
+                                    public void onSuccess(DocumentReference documentReference) {
+                                        Toast.makeText(SignUp.this, "Kayıt işlemi başarılı oldu", Toast.LENGTH_SHORT).show();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(SignUp.this, "Kayıt işlemi başarısız oldu", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                        /*String userId = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+
+                            db.collection("users").document(userId)
+                                    .set(userMap)
+                                    .addOnSuccessListener(aVoid -> {
+                                        // Kullanıcı başarıyla kaydedildi
+                                        // Burada kullanıcıyı uygulamanızın ana ekranına yönlendirebilirsiniz
+                                    })
+                                    .addOnFailureListener(e -> {
+                                        // Firestore'a kayıt başarısız oldu
+                                        Toast.makeText(SignUp.this, "Kayıt işlemi başarısız oldu", Toast.LENGTH_SHORT).show();
+                                    });*/
+
+                    } else {
+                        // Kayıt başarısız oldu
+                        Toast.makeText(SignUp.this, "Kayıt işlemi başarısız oldu", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 
 }
